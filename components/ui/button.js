e5bf95cd1893aefdc2a32f3dc0707bd1280eecb7
Denis <paskin.denis.00@gmail.com>
2024-04-12 11:14:30 +0200

    added a filter form and the filtered events page

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,14 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 function Button(props) {
-  return (
-    <Link className={classes.btn} href={props.link}>
-      {props.children}
-    </Link>
-  );
+  if (props.link)
+    return (
+      <Link className={classes.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+
+  return <button className={classes.btn}>{props.children}</button>;
 }
 
 export default Button;
